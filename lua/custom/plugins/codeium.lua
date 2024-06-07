83879684799d03e5b46bf709fce0c3c45c890617
samaelod <samy.mailoud90@gmail.com>
2024-06-07 15:28:18 +0200

    Customizing

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -2,13 +2,20 @@ return {
 
   {
     'Exafunction/codeium.vim',
-
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    event = 'BufEnter',
     -- NOTE: codeium plugin key bindings
     --set statusline+=%\{...\}%3{codeium#GetStatusString()}
     config = function()
+      -- require('codeium').setup {}
       -- Add to statusline
       -- vim.opt.statusline .. '{...}%3{codeium#GetStatusString()}'
       -- Change '<C-g>' here to any keycode you like.
+      vim.g.codeium_disable_bindings = 1
+      --
       vim.keymap.set('i', '<A-l>', function()
         return vim.fn['codeium#Accept']()
       end, { expr = true, silent = true })
@@ -22,6 +29,7 @@ return {
         return vim.fn['codeium#Clear']()
       end, { expr = true, silent = true })
       --vim.keymap.del('i', '<tab>')
+      vim.api.nvim_call_function('codeium#GetStatusString', {})
     end,
   },
 }
